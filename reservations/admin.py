2ff4018b29d875aa1667121b671586c4a6b31c5e
MinChan Lee <shigatsu970704@gmail.com>
2021-02-22 14:05:30 +0900

    16.Reservation Model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Reservation)
+class AdminReservation(admin.ModelAdmin):
+
+    """ Reservation Admin Definition """
+
+    pass
