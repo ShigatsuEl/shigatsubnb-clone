70c5ce8d2e9fb201a84d97829514838a0ec37853
MinChan Lee <shigatsu970704@gmail.com>
2021-02-25 15:09:58 +0900

    40.Seed Rooms(Amenity, Facility, HouseRule Part)

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -25,7 +25,6 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
-        print(room_types, all_users)
         seeder.add_entity(
             room_models.Room,
             number,
@@ -46,12 +45,32 @@ class Command(BaseCommand):
         # 찾은 room을 토대로 사진들을 생성함
         created_rooms = seeder.execute()
         created_clean = flatten(list(created_rooms.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
         for pk in created_clean:
             room = room_models.Room.objects.get(pk=pk)
-            for i in range(3, random.randint(10, 17)):
+            for i in range(3, random.randint(10, 30)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
+                    # Foreign Key를 가능하게 하기 위함
                     room=room,
                     file=f"room_photos/{random.randint(1, 31)}.webp",
                 )
+            for amenity in amenities:
+                magic_number = random.randint(1, 15)
+                if magic_number % 2 == 0:
+                    # Many to Many Field에 추가하기 위함
+                    room.amenities.add(amenity)
+            for facility in facilities:
+                magic_number = random.randint(1, 15)
+                if magic_number % 2 == 0:
+                    # Many to Many Field에 추가하기 위함
+                    room.facilities.add(facility)
+            for rule in rules:
+                magic_number = random.randint(1, 15)
+                if magic_number % 2 == 0:
+                    # Many to Many Field에 추가하기 위함
+                    room.house_rules.add(rule)
+
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
