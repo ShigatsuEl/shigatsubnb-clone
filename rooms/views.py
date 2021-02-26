2b21873b60aff1c3326d0ddf50f14e3384c5c08f
MinChan Lee <shigatsu970704@gmail.com>
2021-02-26 14:39:42 +0900

    53.Get Page VS Page Method

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,3 @@
-from math import ceil
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from . import models
@@ -6,14 +5,16 @@ from . import models
 
 # View 함수이름은 core/urls.py에서 사용하는 view 이름과 동일해야 한다
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
+    # room_list를 선언한 것까지는 데이터베이스에서 불러오지 않기 때문에 괜찮다
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
+    # Paginator 클래스는 첫번째 인자로 objet_list를 두번째 인자는 페이지 당 몇개의 리스트를 받는지 정함
+    paginator = Paginator(room_list, 10, orphans=5)
+    # get_page 메서드는 하나의 Page object를 반환
+    rooms = paginator.page(int(page))
     # 2번째 인자는 templates이름과 동일해야 한다
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": rooms},
+        context={"page": rooms},
     )
