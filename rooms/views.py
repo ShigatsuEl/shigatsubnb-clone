789c62bd3e0c05d4a1a3bbfe7fc4cc766ee7041f
MinChan Lee <shigatsu970704@gmail.com>
2021-02-26 12:54:16 +0900

    49.Pagination with Limit, Offset and HttpRequest

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,10 @@ from . import models
 
 # View 함수이름은 core/urls.py에서 사용하는 view 이름과 동일해야 한다
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    size = 10
+    limit = size * page
+    offset = limit - size
+    all_rooms = models.Room.objects.all()[offset:limit]
     # 2번째 인자는 templates이름과 동일해야 한다
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
