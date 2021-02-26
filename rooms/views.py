1ebb726526633997d631d5c7d596fbeddcd58caf
MinChan Lee <shigatsu970704@gmail.com>
2021-02-26 12:12:08 +0900

    47.Extending Django Template(Extend and Django-HTML Settings)

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,9 @@
-from datetime import datetime
 from django.shortcuts import render
+from . import models
 
 
+# View 함수이름은 core/urls.py에서 사용하는 view 이름과 동일해야 한다
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()
+    # 2번째 인자는 templates이름과 동일해야 한다
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
