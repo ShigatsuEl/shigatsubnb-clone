df56513345c1b5f0751083bf07a79aead0b5fc6c
MinChan Lee <shigatsu970704@gmail.com>
2021-02-28 10:38:59 +0900

    55.Class Based View

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,12 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
-# View 함수이름은 core/urls.py에서 사용하는 view 이름과 동일해야 한다
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    # room_list를 선언한 것까지는 데이터베이스에서 불러오지 않기 때문에 괜찮다
-    room_list = models.Room.objects.all()
-    # Paginator 클래스는 첫번째 인자로 objet_list를 두번째 인자는 페이지 당 몇개의 리스트를 받는지 정함
-    paginator = Paginator(room_list, 10, orphans=5)
-    # 모든 예외를 처리하고 싶다면 try except를 사용하고 except에 들어갈 인자는 Exception이다.
-    try:
-        # page or get_page 메서드는 하나의 Page object를 반환
-        rooms = paginator.page(int(page))
-        # 2번째 인자는 templates이름과 동일해야 한다
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
