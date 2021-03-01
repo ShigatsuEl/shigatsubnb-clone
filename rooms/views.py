7775e9b09d23ab6d5325d345362062e978fff633
MinChan Lee <shigatsu970704@gmail.com>
2021-03-01 17:05:17 +0900

    68.Filtering Instant, Superhost and Rest Field

--- rooms/views.py
+++ rooms/views.py
@@ -24,6 +24,7 @@ class RoomDetailView(DetailView):
 
 
 def search(requset):
+    # URL에서 가져오는 쿼리들은 대부분 str타입이기 때문에 변형해줘야 한다
     city = requset.GET.get("city", "Anywhere")
     city = str.capitalize(city)
     country = requset.GET.get("country", "KR")
@@ -35,8 +36,8 @@ def search(requset):
     baths = int(requset.GET.get("baths", 0))
     select_amenities = requset.GET.getlist("amenities")
     select_facilities = requset.GET.getlist("facilities")
-    instant_book = requset.GET.get("instant_book", False)
-    super_host = requset.GET.get("super_host", False)
+    instant_book = bool(requset.GET.get("instant_book", False))
+    superhost = bool(requset.GET.get("superhost", False))
 
     form = {
         "city": city,
@@ -50,7 +51,7 @@ def search(requset):
         "select_amenities": select_amenities,
         "select_facilities": select_facilities,
         "instant_book": instant_book,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -65,6 +66,7 @@ def search(requset):
     }
 
     # filter_args = 검색 조건으로 필터링 추가
+    # argument는 Model의 field__조건: 대상 으로 필터링
     filter_args = {}
 
     if city != "Anywhere":
@@ -73,7 +75,39 @@ def search(requset):
     filter_args["country"] = country
 
     if room_type != 0:
+        # Foreign Key를 사용해서 필터링 가능
         filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    print(instant_book, superhost)
+
+    if instant_book is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(select_amenities) > 0:
+        for select_amenity in select_amenities:
+            filter_args["amenities__pk"] = int(select_amenity)
+
+    if len(select_facilities) > 0:
+        for select_facility in select_facilities:
+            filter_args["facilities__pk"] = int(select_facility)
 
     rooms = models.Room.objects.filter(**filter_args)
 
