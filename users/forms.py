e2fd6653647bb78f7d1ba82c16ee11db953504bd
MinChan Lee <shigatsu970704@gmail.com>
2021-03-02 14:23:28 +0900

    76.User Login and Logout(Validating Password)

--- users/forms.py
+++ users/forms.py
@@ -12,16 +12,15 @@ class LoginForm(forms.Form):
     # 지어낸 메서드가 아니며 어떠한 필드를 확인하고 싶다면 clean_(field)로 메서드를 설정한다
     # clean 메서드에서 어떤것도 return하지 않으면 기본적으로 field를 정리한다
     # clean 메서드에서 return하면 그것이 cleaned data가 된다
-    def clean_email(self):
+    # clean 메서드를 썼으면 cleaned data를 꼭 사용하자
+    def clean(self):
         email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-        # cleaned_data 메서드는 field를 정리한 결과물이다
-        print(self.cleaned_data)
-
-    def clean_password(self):
-        print("clean password")
+            self.add_error("email", forms.ValidationError("User does not exist"))
