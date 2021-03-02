aa1458ef7583ed5d4d4c79eb0721604d6a5515d4
MinChan Lee <shigatsu970704@gmail.com>
2021-03-02 13:08:16 +0900

    75.User Login and Logout(Validating Email)

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -7,3 +8,20 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # 지어낸 메서드가 아니며 어떠한 필드를 확인하고 싶다면 clean_(field)로 메서드를 설정한다
+    # clean 메서드에서 어떤것도 return하지 않으면 기본적으로 field를 정리한다
+    # clean 메서드에서 return하면 그것이 cleaned data가 된다
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+        # cleaned_data 메서드는 field를 정리한 결과물이다
+        print(self.cleaned_data)
+
+    def clean_password(self):
+        print("clean password")
--- users/views.py
+++ users/views.py
@@ -8,9 +8,12 @@ class LoginView(View):
     """ Login View Definition """
 
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "shigatsu@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            # cleaned_data는 clean메서드로 정리한 field들의 결과를 보여준다
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
