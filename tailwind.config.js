9af1103879711fdd994afb60d02d7b3d2875ca8e
MinChan Lee <shigatsu970704@gmail.com>
2021-03-07 12:23:36 +0900

    102.Extending TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+        "80vh": "80vh",
+        "100vh": "100vh",
+      },
+      borderRadius: {
+        xl: "1.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
