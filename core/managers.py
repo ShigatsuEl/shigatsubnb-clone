e1005b1901f52385c8f54085fc8368f18723e23b
MinChan Lee <shigatsu970704@gmail.com>
2021-03-20 15:07:49 +0900

    181.Fix CustomModelManager to CustomUserManager(Login Work!)

--- core/managers.py
+++ core/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 # models.blabla.objects.get ..filter ..crete 등을 manager라고 정의한다
@@ -9,3 +10,7 @@ class CustomModelManager(models.Manager):
             return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+
+class CustomUserManager(CustomModelManager, UserManager):
+    pass
