dd3057efce710d5ce64e5c0a29757e2538ee028b
MinChan Lee <shigatsu970704@gmail.com>
2021-03-20 15:09:42 +0900

    182.Fix When On Favs Function Return None Type

--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -7,5 +7,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def on_favs(context, room):
     user = context.request.user
-    list = list_models.List.objects.get_or_none(user=user, name="My Favorite House")
-    return room in list.rooms.all()
+    if user.is_authenticated:
+        the_list = list_models.List.objects.get_or_none(user=user)
+        if the_list is not None:
+            return room in the_list.rooms.all()
+        else:
+            return False
+    else:
+        return False
