0e46ed3dea455423fe29714a8355c98179ad710d
MinChan Lee <shigatsu970704@gmail.com>
2021-03-21 12:40:43 +0900

    186.Django Translation in Reservation Admin and Model

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from core import models as core_models
 
 
@@ -8,8 +9,10 @@ class BookedDay(core_models.TimeStampModel):
 
     """ BookDay Model Definition """
 
-    day = models.DateField()
-    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+    day = models.DateField(_("Day"))
+    reservation = models.ForeignKey(
+        "Reservation", on_delete=models.CASCADE, verbose_name=_("Reservation")
+    )
 
     class Meta:
 
@@ -26,21 +29,27 @@ class Reservation(core_models.TimeStampModel):
     STATUS_CANCELED = "canceled"
 
     STATUS_CHOICES = (
-        (STATUS_PENDING, "Pending"),
-        (STATUS_CONFIRMED, "Confirmed"),
-        (STATUS_CANCELED, "Canceled"),
+        (STATUS_PENDING, _("Pending")),
+        (STATUS_CONFIRMED, _("Confirmed")),
+        (STATUS_CANCELED, _("Canceled")),
     )
 
-    check_in = models.DateField()
-    check_out = models.DateField()
+    check_in = models.DateField(_("Check In"))
+    check_out = models.DateField(_("Check Out"))
     status = models.CharField(
-        max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
+        _("Status"), max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
     )
     guest = models.ForeignKey(
-        "users.User", related_name="reservations", on_delete=models.CASCADE
+        "users.User",
+        related_name="reservations",
+        on_delete=models.CASCADE,
+        verbose_name=_("Guest"),
     )
     room = models.ForeignKey(
-        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+        "rooms.Room",
+        related_name="reservations",
+        on_delete=models.CASCADE,
+        verbose_name=_("Room"),
     )
 
     def __str__(self):
@@ -51,6 +60,7 @@ class Reservation(core_models.TimeStampModel):
         return now >= self.check_in and now <= self.check_out
 
     in_progress.boolean = True
+    in_progress.short_description = _("In Progress")
 
     def is_finished(self):
         now = timezone.now().date()
@@ -60,6 +70,7 @@ class Reservation(core_models.TimeStampModel):
         return is_finished
 
     is_finished.boolean = True
+    is_finished.short_description = _("Is Finished")
 
     def save(self, *args, **kwargs):
         if self.pk is None:
