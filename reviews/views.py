c0ccbbaf94e4aa0bb4e7c82a5358a1e6eddc4c3e
MinChan Lee <shigatsu970704@gmail.com>
2021-03-25 14:07:05 +0900

    208.Make Visible to Responsive User in Reservation Detail View
    +Fix Message Overlap to Header

--- reviews/views.py
+++ reviews/views.py
@@ -1,22 +1,30 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from rooms import models as room_models
+from reservations import models as reservation_models
 from . import forms
 
 
-def create_review(request, room):
+def create_review(request, room, reservation):
 
     """ CreateReview View Definition """
 
     if request.method == "POST":
         form = forms.CreateReviewForm(request.POST)
         room = room_models.Room.objects.get_or_none(pk=room)
-        if not room:
+        reservation = reservation_models.Reservation.objects.get_or_none(pk=reservation)
+        if not room or not reservation:
             return redirect(reverse("core:home"))
         if form.is_valid:
-            review = form.save()
-            review.room = room
-            review.user = request.user
-            review.save()
-            messages.success(request, "Room reviewed")
-            return redirect(reverse("rooms:detail", kwargs={"pk": room.pk}))
+            if reservation.guest == request.user:
+                review = form.save()
+                review.room = room
+                review.user = request.user
+                review.save()
+                messages.success(request, "Room reviewed")
+                return redirect(reverse("rooms:detail", kwargs={"pk": room.pk}))
+            else:
+                messages.error(request, "You can't access reivew")
+                return redirect(
+                    reverse("reservations:detail", kwargs={"pk": reservation.pk})
+                )
