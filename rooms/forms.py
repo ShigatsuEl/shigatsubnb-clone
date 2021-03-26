8c24a3161ec705202c23a678ad6b901a4358109d
MinChan Lee <shigatsu970704@gmail.com>
2021-03-26 12:53:52 +0900

    213.CSS Media Query Search Form

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,36 +1,79 @@
 from django import forms
+from django.core.validators import MinValueValidator
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 from . import models
 
 
 # Djnago Template에서 Form을 만드는 대신 forms.py에서 Form을 간편하게 생성한다
 class SearchForm(forms.Form):
 
-    city = forms.CharField(initial="Anywhere")
-    country = CountryField(default="KR").formfield()
+    city = forms.CharField(
+        initial="Anywhere", widget=forms.TextInput(attrs={"class": "form-btn"})
+    )
+    country = CountryField(default="KR").formfield(
+        widget=CountrySelectWidget(attrs={"class": "form-btn"})
+    )
     # ModelChoiceField는 Select Option과 똑같은 Form 기능이다
     # ModelChoiceField는 Foreign Key에 해당한다
     # ModeleChoiceField는 인자로 QuerySet을 가진다
     room_type = forms.ModelChoiceField(
-        required=False, empty_label="Any Kind", queryset=models.RoomType.objects.all()
+        required=False,
+        empty_label="Any Kind",
+        queryset=models.RoomType.objects.all(),
+        widget=forms.Select(attrs={"class": "form-btn"}),
     )
-    price = forms.IntegerField(required=False)
-    guests = forms.IntegerField(required=False)
-    bedrooms = forms.IntegerField(required=False)
-    beds = forms.IntegerField(required=False)
-    baths = forms.IntegerField(required=False)
-    instant_book = forms.BooleanField(required=False)
-    superhost = forms.BooleanField(required=False)
+    price = forms.IntegerField(
+        required=False,
+        validators=[
+            MinValueValidator(0),
+        ],
+        widget=forms.NumberInput(attrs={"class": "form-btn"}),
+    )
+    guests = forms.IntegerField(
+        required=False,
+        validators=[
+            MinValueValidator(0),
+        ],
+        widget=forms.NumberInput(attrs={"class": "form-btn"}),
+    )
+    bedrooms = forms.IntegerField(
+        required=False,
+        validators=[
+            MinValueValidator(0),
+        ],
+        widget=forms.NumberInput(attrs={"class": "form-btn"}),
+    )
+    beds = forms.IntegerField(
+        required=False,
+        validators=[
+            MinValueValidator(0),
+        ],
+        widget=forms.NumberInput(attrs={"class": "form-btn"}),
+    )
+    baths = forms.IntegerField(
+        required=False,
+        validators=[
+            MinValueValidator(0),
+        ],
+        widget=forms.NumberInput(attrs={"class": "form-btn"}),
+    )
+    instant_book = forms.BooleanField(
+        required=False, widget=forms.CheckboxInput(attrs={"class": "checkbox-input"})
+    )
+    superhost = forms.BooleanField(
+        required=False, widget=forms.CheckboxInput(attrs={"class": "checkbox-input"})
+    )
     # ModelMultipleChoiceField는 Many to Many Field에 해당한다
     amenities = forms.ModelMultipleChoiceField(
         required=False,
         queryset=models.Amenity.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "form-btn w-auto"}),
     )
     facilities = forms.ModelMultipleChoiceField(
         required=False,
         queryset=models.Facility.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "form-btn w-auto"}),
     )
 
 
